ff810f74d7025b6492d2e1d7f721dabb201bc604
assyb447 <assyb447@gmail.com>
2025-12-17 16:27:13 +0900

    [feat] 프로젝트 환경설정 추가

--- src/main/java/com/todoapp/shared_todo/SharedTodoApplication.java
+++ src/main/java/com/todoapp/shared_todo/SharedTodoApplication.java
@@ -7,7 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SharedTodoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SharedTodoApplication.class, args);
-	}
-
+        SpringApplication.run(SharedTodoApplication.class, args);
+        System.out.println("\n" +
+                "=================================================\n" +
+                "🚀 Shared ToDo Application 시작 완료!\n" +
+                "=================================================\n" +
+                "📋 Swagger UI: http://localhost:8080/swagger-ui/index.html\n" +
+                "⭐ JPA Auditing 적용: 자동 시간/사용자 추적\n" +
+                "=================================================\n");
+    }
 }
