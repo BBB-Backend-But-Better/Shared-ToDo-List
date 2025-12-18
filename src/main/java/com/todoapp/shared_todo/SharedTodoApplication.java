49afa89a4e558e5023bff187b6af5d5cafd78b0c
taeju <tjxown0@gmail.com>
2025-12-18 13:50:19 +0900

    [feat] JPA Auditing 추가

--- src/main/java/com/todoapp/shared_todo/SharedTodoApplication.java
+++ src/main/java/com/todoapp/shared_todo/SharedTodoApplication.java
@@ -1,9 +1,10 @@
 package com.todoapp.shared_todo;
 
-import com.todoapp.shared_todo.dto.Users;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SharedTodoApplication {
 
