482aa854698421ca57fe3a3959c9ebe9be0019b2
Myeong-Jin-An <assyv448@daum.net>
2025-12-18 16:30:16 +0900

    [feat] boards 및 todoLists의 테스트 코드 작성

--- src/main/java/com/todoapp/shared_todo/dto/todoLists/TodoUpdateRequestDto.java
+++ src/main/java/com/todoapp/shared_todo/dto/todoLists/TodoUpdateRequestDto.java
@@ -8,9 +8,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor      // JSON 역직렬화 필수
-@AllArgsConstructor     // 테스트 편의 + 빠른 생성
-@Builder                // 부분 필드 테스트 + 가독성
+@NoArgsConstructor      
+@AllArgsConstructor     
+@Builder                
 public class TodoUpdateRequestDto {
 
     @NotBlank(message = "Todo 내용은 필수입니다.")
