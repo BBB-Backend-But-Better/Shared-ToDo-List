9150b5394da4e19e807a2eeef051c611070f514e
taeju <tjxown0@gmail.com>
2025-12-18 13:40:49 +0900

    [feat] assertj 추가

--- src/main/java/com/todoapp/shared_todo/SharedTodoApplication.java
+++ src/main/java/com/todoapp/shared_todo/SharedTodoApplication.java
@@ -1,5 +1,6 @@
 package com.todoapp.shared_todo;
 
+import com.todoapp.shared_todo.dto.Users;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
