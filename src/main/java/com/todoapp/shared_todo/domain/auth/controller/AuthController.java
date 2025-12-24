1f96d5b499275aeb71a11f3220e86b2462f28a67
taeju <tjxown0@gmail.com>
2025-12-24 13:18:51 +0900

    [fix] 컴파일 에러 해결(#16)

--- src/main/java/com/todoapp/shared_todo/domain/auth/controller/AuthController.java
+++ src/main/java/com/todoapp/shared_todo/domain/auth/controller/AuthController.java
@@ -3,7 +3,6 @@ package com.todoapp.shared_todo.domain.auth.controller;
 
 import com.todoapp.shared_todo.domain.auth.dto.LoginRequest;
 import com.todoapp.shared_todo.domain.auth.dto.SignupRequest;
-import com.todoapp.shared_todo.dto.users.UserResponseDto;
 import com.todoapp.shared_todo.domain.user.entity.Users;
 import com.todoapp.shared_todo.domain.auth.service.AuthService;
 import jakarta.validation.Valid;
@@ -19,39 +18,5 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class AuthController {
 
-    private final AuthService authService;
 
-    //사용자 회원가입 API
-    //post /auth/signup
-    @PostMapping("/signup")
-    public ResponseEntity<UserResponseDto> signup(@Valid @RequestBody SignupRequest dto) {
-
-        Users userId = authService.signUp(dto);
-
-        UserResponseDto response = UserResponseDto.builder()
-                .id(userId.getId())
-                .loginId(dto.getLoginId())
-                .build();
-
-
-        return ResponseEntity
-                .status(HttpStatus.CREATED)
-                .body(response);
-    }
-
-
-    /**
-     * 사용자 로그인
-     * jwt 인틍 토큰 작업 해야됨
-     * post /auth/login
-     */
-
-    @PostMapping("/login")
-    public ResponseEntity<Map<String,Object>> login(@Valid @RequestBody LoginRequest dto) {
-
-    }
-
-    //로그아웃, 리프래시 토큰 폐기
-
-    
 }
--- src/test/java/com/todoapp/shared_todo/dto/users/LoginRequestTest.java
+++ src/test/java/com/todoapp/shared_todo/dto/users/LoginRequestTest.java
@@ -14,25 +14,4 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class LoginRequestTest {
 
-    private static Validator validator;
-
-    @BeforeAll
-    static void beforeAll() {
-        validator = Validation.buildDefaultValidatorFactory().getValidator();
-    }
-
-    @Test
-    @DisplayName("login 정상 입력 - 검증 통과")
-    void LoginRequestDtoTest() {
-        LoginRequestDto loginRequestDto = new LoginRequestDto("test", "test");
-        Set<ConstraintViolation<LoginRequestDto>> validate = validator.validate(loginRequestDto);
-
-        Assertions.assertTrue(validate.isEmpty());
-
-    }
-
-    @Test
-    @DisplayName("login이 blank ")
-
-
 }
