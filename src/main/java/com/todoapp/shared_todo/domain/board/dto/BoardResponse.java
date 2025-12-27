528c7da1ab0069ba773198acdac3d92b2a66df99
Myeong-Jin-An <assyv448@daum.net>
2025-12-27 20:02:38 +0900

    [feat/board]: Board 도메인 Entity, DTO, Repository, Service, Controller 개발(#33)

--- src/main/java/com/todoapp/shared_todo/domain/board/dto/BoardResponse.java
+++ src/main/java/com/todoapp/shared_todo/domain/board/dto/BoardResponse.java
@@ -1,7 +1,15 @@
 package com.todoapp.shared_todo.domain.board.dto;
 
+import lombok.*;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // 직렬화 대비
+@AllArgsConstructor(access = AccessLevel.PRIVATE)  // 빌더에서만 사용
 public class BoardResponse {
 
+    private Long id;
+    private String title;
+    private Long authorId;
+    private Float completionRate;
 }
-
-
