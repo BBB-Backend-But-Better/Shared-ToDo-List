9383e7815f602ffe9cff7f42d2405c3819787a16
YuJeong <qordbwjd96@gmail.com>
2025-12-28 14:58:18 +0900

    [feat] BoardMemberRepository 수정 (#35)

--- src/main/java/com/todoapp/shared_todo/domain/boardMember/repository/BoardMemberRepository.java
+++ src/main/java/com/todoapp/shared_todo/domain/boardMember/repository/BoardMemberRepository.java
@@ -1,8 +1,6 @@
 package com.todoapp.shared_todo.domain.boardMember.repository;
 
-import com.todoapp.shared_todo.domain.board.entity.Board;
 import com.todoapp.shared_todo.domain.boardMember.entity.BoardMember;
-import com.todoapp.shared_todo.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,18 +10,29 @@ import java.util.Optional;
 @Repository
 public interface BoardMemberRepository extends JpaRepository<BoardMember, Long> {
 
-    // 보드와 유저로 BoardMember 조회
-    Optional<BoardMember> findByBoardAndUser(Board board, User user);
+    /**
+     * 특정 보드에 속한 모든 멤버 조회
+     * - 보드 멤버 목록 API에서 사용
+     */
+    List<BoardMember> findByBoardId(Long boardId);
 
-    // 보드 ID와 유저 ID로 BoardMember 조회
+    /**
+     * 특정 보드에 특정 유저가 멤버로 존재하는지 조회
+     * - 권한 체크 (OWNER / GUEST 공통)
+     * - 초대 수락 시 중복 참여 방지
+     */
     Optional<BoardMember> findByBoardIdAndUserId(Long boardId, Long userId);
 
-    // 보드의 모든 멤버 조회
-    List<BoardMember> findByBoard(Board board);
+    /**
+     * 특정 보드에 특정 유저가 이미 참여 중인지 여부 확인
+     * - 초대 생성 / 수락 전 검증용
+     */
+    boolean existsByBoardIdAndUserId(Long boardId, Long userId);
 
-    // 유저가 참여한 모든 보드 조회
-    List<BoardMember> findByUser(User user);
-
-    // 보드 ID로 모든 멤버 조회
-    List<BoardMember> findByBoardId(Long boardId);
+    /**
+     * 특정 보드에서 특정 유저를 멤버에서 제거
+     * - OWNER가 다른 멤버 강퇴
+     * - GUEST 본인 나가기
+     */
+    void deleteByBoardIdAndUserId(Long boardId, Long userId);
 }
